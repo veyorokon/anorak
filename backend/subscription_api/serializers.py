44e53742106b77fd841a06ae1d5cac30d2a11058
Vahid <veyorokon@gmail.com>
2018-10-22 16:13:16 -0700

    Squads can now be created along with appropriate stripe products and pricing plans. The ability to delete and delete stripe plans also works.

--- backend/subscription_api/serializers.py
+++ backend/subscription_api/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
 from . models import *
 
+class StripePlanSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = StripePlan
+        fields = '__all__'
+
+
 class SquadSerializer(serializers.ModelSerializer):
 
     class Meta:
