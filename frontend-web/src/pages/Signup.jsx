1b01c68f4051184981b356a496c028fc3efcf57d
Ben Obringer <obringer.20@gmail.com>
2018-11-03 13:55:23 -0700

    Complete rough draft of signup page #30

---
+++ frontend-web/src/pages/Signup.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import SignupForm from '../components/SignupForm';
+
+const styles = theme => ({
+  layout: {
+    width: 'auto',
+    display: 'block', // Fix IE 11 issue.
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: 'auto',
+      marginRight: 'auto'
+    }
+  }
+});
+
+function Signup(props) {
+  const { classes } = props;
+
+  return (
+    <main className={classes.layout}>
+      <SignupForm />
+    </main>
+  );
+}
+
+Signup.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Signup);
