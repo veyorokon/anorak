086d3254b07d261944b80d204868fbbb9c6a805b
Ben Obringer <obringer.20@gmail.com>
2018-11-04 00:03:18 -0700

    Change theme colors

--- frontend-web/src/App.js
+++ frontend-web/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import Create from './pages/Create';
@@ -7,7 +8,16 @@ import Dashboard from './pages/Dashboard';
 import Join from './pages/Join';
 import Signup from './pages/Signup';
 
-// import './App.css';
+const theme = createMuiTheme({
+  palette: {
+    secondary: {
+      main: '#f8be00'
+    },
+    primary: {
+      main: '#2e2836'
+    }
+  }
+});
 
 class App extends React.Component {
   render() {
@@ -15,12 +25,12 @@ class App extends React.Component {
       <React.Fragment>
         <CssBaseline />
         <Router>
-          <div>
+          <MuiThemeProvider theme={theme}>
             <Route path="/" exact component={Dashboard} />
             <Route path="/create/" component={Create} />
             <Route path="/join/" component={Join} />
             <Route path="/signup" component={Signup} />
-          </div>
+          </MuiThemeProvider>
         </Router>
       </React.Fragment>
     );
