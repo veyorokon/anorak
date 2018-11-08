dc85ed81ca8142005e675d6796434bb0506cb48d
Vahid <veyorokon@gmail.com>
2018-11-07 21:34:34 -0800

    Added a popup modal to the squad cards.

--- frontend-web/src/components/SquadCard.jsx
+++ frontend-web/src/components/SquadCard.jsx
@@ -4,8 +4,8 @@ import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import SquadCardModal from './SquadCardModal';
 
 const styles = {
   card: {
@@ -31,9 +31,7 @@ function SquadCard(props) {
         <Typography component="p">{props.description}</Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">
-          <Typography style={{ color: '#138A36' }}>Access</Typography>
-        </Button>
+        <SquadCardModal />
       </CardActions>
     </Card>
   );
---
+++ frontend-web/src/components/SquadCardModal.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import withMobileDialog from '@material-ui/core/withMobileDialog';
+
+class SquadCardModal extends React.Component {
+  state = {
+    open: false
+  };
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { fullScreen } = this.props;
+
+    return (
+      <div>
+        <Button style={{ color: '#138A36' }} onClick={this.handleClickOpen}>
+          Access
+        </Button>
+        <Dialog
+          fullScreen={fullScreen}
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="responsive-dialog-title"
+        >
+          <DialogTitle id="responsive-dialog-title">
+            {'Squad Title Here'}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Here is something about this squad
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Disagree
+            </Button>
+            <Button onClick={this.handleClose} color="primary" autoFocus>
+              Agree
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+SquadCardModal.propTypes = {
+  fullScreen: PropTypes.bool.isRequired
+};
+
+export default withMobileDialog()(SquadCardModal);
