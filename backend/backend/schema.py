f3e052534c2cac9e28a678d3c166764a34e72c37
Ben Obringer <obringer.20@gmail.com>
2018-11-10 12:26:08 -0800

    Fix server bugs

--- backend/backend/schema.py
+++ backend/backend/schema.py
@@ -10,7 +10,7 @@ class Mutations(CoreMutations, SubscriptionMutations, graphene.ObjectType):
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
 
-class Query(CoreQuery, SubscriptionQuery):
+class Query(CoreQuery):
     pass
-    
+
 schema = graphene.Schema(query=Query, mutation=Mutations)
