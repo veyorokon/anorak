2a05854230dd927ade139f18866d1cb3a4897b54
Vahid <veyorokon@gmail.com>
2018-11-09 19:54:00 -0800

    Added graphene with permissions to user data.

--- backend/core/schema.py
+++ backend/core/schema.py
@@ -1,7 +1,7 @@
 import graphene 
 from graphene_django.types import DjangoObjectType
 from . models import * 
-from graphql_jwt.decorators import login_required
+from graphql_jwt.decorators import login_required, staff_member_required
 
 
 class UserType(DjangoObjectType):
@@ -14,21 +14,33 @@ class StripeCustomerType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     all_users = graphene.List(UserType, token=graphene.String(required=True))
-    user = graphene.Field(UserType, id=graphene.Int())
-    
-    def resolve_user(self, info, id, **kwargs):
-        return User.objects.get(pk=id)
+    user = graphene.Field(UserType, token=graphene.String(required=True))
+    all_stripe_customers = graphene.List(StripeCustomerType)
     
     @login_required
+    def resolve_user(self, info, token, **kwargs):
+        return info.context.user
+    
+    @staff_member_required
     def resolve_all_users(self, info, token, **kwargs):
         return User.objects.all()
-
+        
 
 """
 query allUsers($token: String!) {
   allUsers(token: $token) {
     id
     email
+  }
+}
+
+
+query user($token: String!) {
+  user(token: $token) {
+    id
+    email
+    firstName
+    lastName
   }
 }
 """
