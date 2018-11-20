7c011cf7bb0bf27cf30ce111d323b28d7e33b8b4
Vahid <veyorokon@gmail.com>
2018-11-19 16:06:58 -0800

    Squad search using Graphene works.

--- backend/subscription_api/graphene/query.py
+++ backend/subscription_api/graphene/query.py
@@ -1,4 +1,13 @@
 import graphene 
+from . types import * 
+from subscription_api.models import Squad
+from django.contrib.postgres.search import SearchVector
+
 
 class Query(graphene.ObjectType):
-    pass
+    squad_search = graphene.List(RestrictedSquadType, text=graphene.String(required=True))
+    
+    
+    def resolve_squad_search(self, info, text, **kwargs):
+        return Squad.objects.filter(description__icontains=text)
+        
