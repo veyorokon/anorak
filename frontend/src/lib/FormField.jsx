f46ce8deb8e8a3d5e0273658f96b2f6767915280
Ben Obringer <obringer.20@gmail.com>
2018-11-21 23:46:22 -0800

    Add documentation to form components

--- frontend/src/lib/FormField.jsx
+++ frontend/src/lib/FormField.jsx
@@ -6,8 +6,17 @@ const MaterialUiInputComponent = ({ field, form, ...props }) => (
   <TextField id={field.name} {...field} {...props} />
 );
 
+/**
+ * A combination of a formik field and material-ui TextField so that we can utilize
+ * formik while styling our field at the same time.
+ * @constructor
+ */
 function FormField(props) {
   return <FormikField component={MaterialUiInputComponent} {...props} />;
 }
+FormField.props = {
+  ...FormikField.PropTypes,
+  ...TextField.PropTypes
+};
 
 export default FormField;
