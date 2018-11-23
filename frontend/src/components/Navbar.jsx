143145daa4a3a6cbeddbaaee67d29c9482a12bd5
Ben Obringer <obringer.20@gmail.com>
2018-11-23 14:16:25 -0800

    Improve navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,66 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { fade } from '@material-ui/core/styles/colorManipulator';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
-import InputBase from '@material-ui/core/InputBase';
-import SearchIcon from '@material-ui/icons/Search';
+import IconButton from '@material-ui/core/IconButton';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import { Link } from 'react-router-dom';
 
+import Logo from './Logo';
+
 const styles = theme => ({
-  button: {
-    marginLeft: 16
-  },
-  grow: {
-    flexGrow: 1
-  },
   toolbar: {
     display: 'flex',
     justifyContent: 'space-between'
-  },
-  navbarLeft: {
-    display: 'flex'
-  },
-  search: {
-    flexGrow: 1,
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25)
-    },
-    marginLeft: 36,
-    width: 'auto'
-  },
-  searchIcon: {
-    width: theme.spacing.unit * 9,
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  inputRoot: {
-    color: 'inherit',
-    width: '100%'
-  },
-  inputInput: {
-    paddingTop: theme.spacing.unit,
-    paddingRight: theme.spacing.unit,
-    paddingBottom: theme.spacing.unit,
-    paddingLeft: theme.spacing.unit * 10,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: 120,
-      '&:focus': {
-        width: 200
-      }
-    }
   }
 });
 
@@ -69,54 +22,27 @@ function Navbar(props) {
   return (
     <AppBar position="static">
       <Toolbar className={classes.toolbar}>
-        <div className={classes.navbarLeft}>
-          <Typography variant="h5" color="inherit" noWrap>
-            Squad
-          </Typography>
-          <Typography variant="h5" color="secondary" noWrap>
-            Up
-          </Typography>
-          <Button
-            component={Link}
-            to="/create"
-            variant="outlined"
-            color="secondary"
-            size="small"
-            className={classes.button}
-          >
-            Create
-          </Button>
-          <Button
-            variant="outlined"
-            color="secondary"
-            size="small"
-            className={classes.button}
-          >
-            Join
-          </Button>
-        </div>
-
-        <div className={classes.search}>
-          <div className={classes.searchIcon}>
-            <SearchIcon />
-          </div>
-          <InputBase
-            placeholder="Search…"
-            classes={{
-              root: classes.inputRoot,
-              input: classes.inputInput
-            }}
-          />
-        </div>
-
         <Button
           component={Link}
-          to="/account"
+          to="/create"
+          variant="outlined"
           color="secondary"
-          className={classes.button}
+          size="small"
+          className={classes.createButton}
         >
-          Account
+          Create Squad
         </Button>
+
+        <Logo />
+
+        <IconButton
+          color="secondary"
+          component={Link}
+          to="/account"
+          aria-label="Go to account page"
+        >
+          <AccountCircleIcon />
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
