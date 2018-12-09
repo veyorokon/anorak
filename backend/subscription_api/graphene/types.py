eb4161e376b6a48a5e59975c49284c7eb33f8039
Vahid <veyorokon@gmail.com>
2018-12-08 19:38:46 -0800

    Added new owner field to squad query.

--- backend/subscription_api/graphene/types.py
+++ backend/subscription_api/graphene/types.py
@@ -5,15 +5,20 @@ from subscription_api.models import *
 class SquadType(DjangoObjectType):
     
     current_user_status = graphene.String()
+    owner = graphene.String()
     
     class Meta:
         model = Squad
-        exclude_fields = ['owner', 'secret', 'date_created', 'date_modified', 'stripe_plan']
+        exclude_fields = ['secret', 'date_created', 'date_modified', 'stripe_plan']
     
     def resolve_members(self, info):
         if info.context.user == self.owner:
             return self.members
         return None
+        
+    
+    def resolve_owner(self, info):
+        return '{0} {1}'.format(self.owner.first_name, self.owner.last_name)
         
     
     def resolve_current_user_status(self, info):
