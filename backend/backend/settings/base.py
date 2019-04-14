964ab31b2a0b3f5d284c42e4600fc781c885d15a
Vahid <veyorokon@gmail.com>
2019-04-13 23:05:34 -0700

    Fixed auto logout

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -99,7 +99,10 @@ TAX_JAR_KEY = environ_setting("TAX_JAR_KEY")
 # }
 
 
-SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_ENGINE='django.contrib.sessions.backends.db'
+SESSION_COOKIE_NAME='sessionid'
+SESSION_EXPIRE_AT_BROWSER_CLOSE=False
+SESSION_COOKIE_SECURE=True
 
 # # Other Celery settings
 # CELERY_RESULT_BACKEND = CELERY_BROKER_URL
